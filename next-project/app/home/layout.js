1bbb015399288dfe26f0ec79dc0514c224c52368
altofa7y <a.a.h.f.t2010@gmail.com>
2025-08-18 21:45:24 +0300

    new good commit 2

---
+++ next-project/app/home/layout.js
@@ -0,0 +1,10 @@
+export default function dashboardLayout({children}){
+    return(
+       <>
+        <h1>that is dashboardLayout</h1>
+        <div>
+            {children}
+        </div>
+       </>
+    )
+}
