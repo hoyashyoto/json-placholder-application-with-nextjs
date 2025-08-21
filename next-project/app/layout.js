19a6a51de202c6344bf7e6dbf272151a173418d0
altofa7y <a.a.h.f.t2010@gmail.com>
2025-08-21 14:39:47 +0300

    json placholder with nextjs 0.1

--- next-project/app/layout.js
+++ next-project/app/layout.js
@@ -1,8 +1,20 @@
 import '../styles/global.css';
+const metadata = {
+  title:'Posts'
+  ,
+  description:'post page'
+  ,
+  icons:{
+    icon:'/favicon.ico'
+  }
+}
 export default function RootLayout({ children }) {
   
     return (
       <html lang="en">
+        <head>
+        <link rel="icon" href="/favicon.ico?v=2" />
+        </head>
         <body style={{height:'100vh'}}>
         {children}
         </body>
--- next-project/app/post/[post]/page.jsx
+++ next-project/app/post/[post]/page.jsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react"
 import FechData from "./FetchData"
+import Link from "next/link"
 export default async function post({params}) {
     const {post}=await params
     const loading = (
@@ -9,6 +10,9 @@ export default async function post({params}) {
     )
     return(
         <div  style={{height:'100px',width:'100%',borderRadius:'10px'}}>
+            <Link href={'/'}>
+                <button style={{margin:'4px',borderRadius:'0',background:'transparent',border:0,borderBottom:'1px black solid',fontSize:'32px'}}>&#8592;</button>
+            </Link>
             <h1>Post with id({post})</h1>
 
                 <FechData postId={post}/>
